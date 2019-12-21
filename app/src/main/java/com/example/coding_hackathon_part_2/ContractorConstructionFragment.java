297f2ae238eda2590ba1c01efee335676e0d0118
LavishGulati <lavish00gulati@gmail.com>
2019-12-21 11:26:39 +0530

    Added ConstructionDetail activity

---
+++ app/src/main/java/com/example/coding_hackathon_part_2/ContractorConstructionFragment.java
@@ -0,0 +1,31 @@
+package com.example.coding_hackathon_part_2;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ContractorConstructionFragment extends Fragment {
+
+
+    public ContractorConstructionFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_contractor_construction, container, false);
+    }
+
+}
